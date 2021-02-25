19b1b02e58175da6e1e7081fce24af52461a321b
99003727 <shiva.vankadhara@ltts.com>
2021-02-25 14:20:04 +0530

    final code with out class

--- shiva.py
+++ shiva.py
@@ -1,41 +1,38 @@
-a=str(input("give the string:"))
-with open('C:\\Users\\99003727\\Documents\\GitHub\\python-module-shiva\\pythonminiproject.txt', 'r') as infile:
-    for ch in ['\\n','`','*','_','{','}','[',']','(',')','>','#','+','-','.','!','$','\'','@',str([0-9]),'(',')',',','<']:
-        if ch in infile:
-            infile=infile.replace(ch,"")
-    lines = infile.readlines()
-    
-with open('C:\\Users\\99003727\\Documents\\GitHub\\python-module-shiva\\miniproject_1.txt', 'w') as file_output:
-    
-
-    count=0
-    words=[]
-    word_index=[]
-    before_words=[]
-    after_words=[]
-    for x in lines: #accesing the elements in lines
-        for word in x.split(): # accesing the word in line
-            if a.upper() in word.upper():
-                   count+=1                                #checking the word in line and increasing the count
-           #if word.isalpha():
-            word.split("[\\s@&.? ()$+-]+") #splitting the word which has special characters
-            words.append(word.upper())
-           
-    for i in range(len(words)):                     # getting the range of the repeated input in words list
-        if a.upper() in words[i].upper():
-            word_index.append(i)              
-
-    file_output.write("count of the given word:\n"+str(count))
-    file_output.write(str(words))
-    #file_output.write(str(word_index))
-    #file_output.write(str(len(word_index)))
-    for i in word_index:                            #getting the previous and after input by accessing range
-        before_words.append(words[i-1])
-        after_words.append(words[i+1])
-    file_output.write("\nbeforewords:\n"+str(before_words))
-    file_output.write("\nafterwords:\n"+str(after_words))
-    file_output.write("\ncount of before words:\n"+str(len(before_words)))
-    file_output.write("\ncount of after words:\n"+str(len(after_words)))
-
-    
-    
+class userinput:
+    def __init__(self,number,input_string):
+        self.number = number
+        self.input_string = input_string
+    def userno.(self,number):
+        return self.number = number
+    def userstring(self,input_string):
+        return self.input_string = input_string
+number = int(input("How many words: "))
+for userinputcount in range(int(number)):
+    input_string = str(input("give the string:"))
+    path = 'C:\\Users\\99003727\\Documents\\GitHub\\python-module-shiva\\'
+    with open('C:\\Users\\99003727\\Documents\\GitHub\\python-module-shiva\\pythonminiproject.txt', 'r') as infile:
+        lines = infile.readlines()
+    with open(path+input_string+'.txt', 'w') as file_output:
+        count = 0
+        words = []
+        word_index = []
+        before_words = []
+        after_words = []
+        for x in lines:  # accesing the word in line
+            for word in x.split():   # accesing the elements in lines
+                if input_string.upper() in word.upper():
+                    count += 1  # checking the word and increasing the count
+                word.split("[\\s@&.? ()$+-]+")  # splitting the word
+                words.append(word.upper())
+        for i in range(len(words)):  # getting the range of the repeated input
+                if input_string.upper() in words[i].upper():
+                    word_index.append(i)
+        file_output.write("The word being find is: "+input_string)
+        file_output.write("\ncount of the given word:\n"+str(count))
+        for i in word_index:    # getting the previous and after input
+            before_words.append(words[i-1])
+            after_words.append(words[i+1])
+        file_output.write("\nbeforewords:\n"+str(before_words))
+        file_output.write("\nafterwords:\n"+str(after_words))
+        file_output.write("\ncount of before words:\n"+str(len(before_words)))
+        file_output.write("\ncount of after words:\n"+str(len(after_words)))
